74d18f2856f85c6f7cbe829336495ec1a7417643
rich <richcherng@gmail.com>
2017-11-14 22:16:10 -0800

    set up compile route

--- app.js
+++ app.js
@@ -8,13 +8,26 @@
 
 const EXPRESS = require('express');
 // const HTTP = require('http');
-const ROUTER = require('./router.js');
+// const ROUTER = require('./router.js');
 const BODY_PARSER = require('body-parser');
+const EXPRESSBRUTE = require('express-brute');
 
-let port = 80;
+
+let port = 3000;
+let store = new EXPRESSBRUTE.MemoryStore(); // stores state locally, don't use this in production 
+let bruteforce = new EXPRESSBRUTE(store, {
+	freeRetries: 50,
+	lifetime:3600
+});
+
 
 const app = EXPRESS();
 
+
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
 app.use(
 	BODY_PARSER.urlencoded({
 		parameterLimit: 100000000,
@@ -23,14 +36,38 @@ app.use(
 	})
 );
 
-// app.use(EXPRESS.static(__dirname));
-// app.use(EXPRESS.bodyParser());
 
-ROUTER(app);
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(BODY_PARSER.json());
 
-// app.get('/', (req, res) => {
-// 	res.send('Hello World!');
-// });
+
+app.use( (_req, _res, _next) => {
+	let message = _req.method + " " + _req.url
+	// log('${_req.method} ${_req.url}', _req);
+	log('use',message, _req);
+	_next();	
+});
+
+
+
+app.get('/', (_req, _res) => {
+	_res.json({ message: 'This is the REST API'});
+
+});
+
+
+app.post('/compile', bruteforce.prevent, (_req, _res)=>{
+
+	console.log(_req.body);
+	let language = _req.body.language
+	_res.send({test:{
+		language
+	}})
+});
+
+
 
 let server = app.listen(port, (error)=>{
 
@@ -46,4 +83,12 @@ let server = app.listen(port, (error)=>{
 
 exports.closeServer = function(){
 	server.close();
+}
+
+
+
+
+function log(_function,_message, _request) {
+  // console.log('app: ', _message);
+  console.log('app: [%s]: %s', _function, _message);
 }
