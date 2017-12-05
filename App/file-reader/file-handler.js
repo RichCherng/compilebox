07df3cc3938a4b27466e87401d6acb843c469682
rich <richcherng@gmail.com>
2017-12-04 19:32:02 -0800

    fixed wrong upload files location

--- App/file-reader/file-handler.js
+++ App/file-reader/file-handler.js
@@ -24,25 +24,25 @@ FileHandler.prototype.parse = function(_req){
 	let form = new FORMIDABLE.IncomingForm();
 	
 	// store all uploads in the /uploads directory	
-	form.uploadDir = PATH.join(__dirname, '/uploads');
+	form.uploadDir = PATH.join(__dirname, '../uploads');
 
 	// Include the extensions of the original files
 	form.type = true;
 
 	
 	form.parse(_req, (err, fields, file) => {
-
+		console.log("testttttttt");
 		if(err){
 			console.log(err);
 		}
 		// console.log(_req.body);	
 		// console.log(file.language);
-		console.log(file.source.name);
+		console.log(file);
 		console.log(file.source.path);
 
 		// console.log(file.input.name);
 		// console.log(file.input.path);
-	
+		
 		FS.rename(file.source.path, PATH.join(form.uploadDir, file.source.name), (err)=>{
 			
 			if (err) throw err;
